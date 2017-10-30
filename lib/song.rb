319e5d37ca479179c3b4389c94791df03da9c88c
David Kuruvilla <dapawn@gmail.com>
2017-10-30 15:34:20 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,19 @@
 class Song
-
+  @@song_count = 0
+  
   attr_accessor :name, :artist
 
   def initialize(name)
     @name = name
+    @@song_count += 1
   end
 
   def artist_name
     self.artist.name if self.artist
   end
 
+  def song_count
+    @@song_count
+  end
 
 end
